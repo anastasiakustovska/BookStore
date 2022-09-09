e3cdc079ec77f43da8ab779a4a93fc41ea8e2abb
anastasiakustovska <anastasiukraine@gmail.com>
2022-09-09 20:21:29 +0300

    add slider

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     ABOUT
+    <ParallaxSlider :background-url="'https://cdn.serif.com/affinity/img/photo/home/0121/og-photo-200120210858.jpg'"/>
   </div>
 </template>
 
 <script>
+import ParallaxSlider from "~/components/Slider/ParallaxSlider";
+
 export default {
+  components: {ParallaxSlider},
   layout: 'index',
   name: "About"
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
+    <ParallaxSlider :background-url="'https://cdn.mos.cms.futurecdn.net/AT9Bcvo94cTMGmESvL4JCK.jpg'" />
   </div>
 </template>
 
 <script>
+
 import { mapMutations } from 'vuex';
+import ParallaxSlider from "~/components/Slider/ParallaxSlider";
 
 export default {
+  components: {ParallaxSlider},
   layout: 'index',
   name: 'IndexPage',
+
 
   computed: {
   },
